8e4113f550b148093f7a271fd7d3014fde51b4ee
username <useremail>
2023-08-26 08:44:51 -0700

    CourseProjectPhase3

--- CourseProjectPhase3.py
+++ CourseProjectPhase3.py
@@ -34,27 +34,55 @@ def printinfo(EmpDetailList):
     
     for EmpList in EmpDetailList:
         
-    fromdate = EmpList[0]
-    todate = EmpList[1]
-    empname = EmpList[2]
-    hours = EmpList[3]
-    hourlyrate = EmpList[4]
-    taxrate = EmpList[5]
+        fromdate = EmpList[0]
+        todate = EmpList[1]
+        empname = EmpList[2]
+        hours = EmpList[3]
+        hourlyrate = EmpList[4]
+        taxrate = EmpList[5]
     
-    grosspay, incometax, netpay = CalcTaxAndNetPay(hours, hourlyrate, taxrate)
-    print(fromdate, todate, empname, f"{hours:,.2f}", f"{hourlyrate:,.2f}", f"{grosspay:,.2f}", f"{taxrate:,.1%}", f"{incometax:,.2f}", f"{netpay:,.2f}")
+        grosspay, incometax, netpay = CalcTaxAndNetPay(hours, hourlyrate, taxrate)
+        print(fromdate, todate, empname, f"{hours:,.2f}", f"{hourlyrate:,.2f}", f"{grosspay:,.2f}", f"{taxrate:,.1%}", f"{incometax:,.2f}", f"{netpay:,.2f}")
     
-    TotEmployees += 1
-    TotHours += hours
-    TotGrossPaay += grosspay
-    TotTax += incometax
-    TotNetPay += netpay
+        TotEmployees += 1
+        TotHours += hours
+        TotGrossPaay += grosspay
+        TotTax += incometax
+        TotNetPay += netpay
     
-EmpTotals["TotEmp"] = TotEmployees
-EmpTotals["TotHrs"] = TotHours
-EmpTotals["TotGrossPay"] = TotGrossPay
-EmpTotals["TotTax"] = TotTax
-EmpTotals["TotNetPay"] = TotNetPay
+    EmpTotals["TotEmp"] = TotEmployees
+    EmpTotals["TotHrs"] = TotHours
+    EmpTotals["TotGrossPay"] = TotGrossPay
+    EmpTotals["TotTax"] = TotTax
+    EmpTotals["TotNetPay"] = TotNetPay
+    
+def PrintTotals(EmpTotals):
+    print()
+    print(f"Total number of employees: {EmpTotals['TotEmp']}")
+    print(f"Total hours worked: {EmpTotals['TotHrs']}")
+    print(f"Total gross pay: {EmpTotals['TotGrossPay']:,.2f}")
+    print(f"Total income tax: {EmpTotals['TotTax']:,.1%}")
+    print(f"Total net pay: {EmpTotals['TotNetPay']:,.2f}")
+    
+def WriteEmployeeInformation(employee):
+    file = open("employeeinfo.txt", "a")
+    file.write('{}|{}|{}|{}|{}|{}\n'.format(employee[0], employee[1], employee[2], employee[3], employee[4], employee[5]))
+    
+def GetFromDate():
+    valid = False
+    fromdate = ""
+    
+    while not valid:
+        fromdate = input("Enter from date (mm/dd/yyyy): ")
+        if (len(fromdate.split('/')) !=3 and fromdate.upper() != 'ALL'):
+            print("Invalid date format")
+        else:
+            valid = True 
+            
+    return fromdate 
+
+def ReadEmployeeInformation(fromdate):
+    
 
 
     
